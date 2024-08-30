1c3a9edf0110c08f4e48fd44acc1120ab0d9c316
awesomedev82 <awesomedev08@gmail.com>
2024-08-30 13:02:54 -0700

    custom keyboard

--- lib/screens/create_announcement/share_first_screen.dart
+++ lib/screens/create_announcement/share_first_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 import 'package:workhouse/components/app_input.dart';
 import 'package:workhouse/utils/constant.dart';
 
@@ -15,37 +16,116 @@ class ShareFirstScreen extends StatefulWidget {
 }
 
 class _ShareFirstScreenState extends State<ShareFirstScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              Container(
-                height: 300,
-                child: TextField(
-                  decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color(0xFFDDDDDD), width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color(0xFFDDDDDD), width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
+  final FocusNode _nodeText = FocusNode();
+
+  /// Creates the [KeyboardActionsConfig] to hook up the fields
+  /// and their focus nodes to our [FormKeyboardActions].
+  KeyboardActionsConfig _buildConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: true,
+      actions: [
+        KeyboardActionsItem(
+          focusNode: _nodeText,
+          displayDoneButton: false,
+          displayArrows: false,
+          toolbarAlignment: MainAxisAlignment.spaceBetween,
+          toolbarButtons: [
+            //button 1
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  width: 32,
+                  height: 32,
+                  margin: EdgeInsets.fromLTRB(24, 0, 0, 0),
+                  child: Image.asset("assets/images/Camera.png"),
+                ),
+              );
+            },
+            //button 2
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  width: 32,
+                  height: 32,
+                  // margin: EdgeInsets.fromLTRB(24, 0, 0, 0),
+                  child: Image.asset("assets/images/Gallery.png"),
+                ),
+              );
+            },
+            //button
+            (node) {
+              return GestureDetector(
+                child: Container(width: 1),
+              );
+            },
+            //button
+            (node) {
+              return GestureDetector(
+                child: Container(width: 1),
+              );
+            },
+            //button
+            (node) {
+              return GestureDetector(
+                child: Container(width: 1),
+              );
+            },
+            //button 3
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  margin: EdgeInsetsDirectional.symmetric(horizontal: 24),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Text(
+                    "Continue",
+                    style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        height: 1.21,
+                      ),
                     ),
                   ),
+                ),
+              );
+            },
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyboardActions(
+      config: _buildConfig(context),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.text,
+                focusNode: _nodeText,
+                decoration: InputDecoration(
+                  hintText: "Input Number with Toolbar Buttons",
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Input Number with Custom Footer",
                 ),
               ),
             ],
