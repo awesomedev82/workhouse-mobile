2b4d0d0b62ccbc3a347b7ba6b1000010ee24cd9c
awesomedev82 <awesomedev08@gmail.com>
2024-08-31 07:42:24 -0700

    Replacement

--- lib/screens/create_announcement/share_first_screen.dart
+++ lib/screens/create_announcement/share_first_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:keyboard_actions/keyboard_actions.dart';
 import 'package:workhouse/components/app_input.dart';
 import 'package:workhouse/utils/constant.dart';
+import 'package:multi_image_picker_plus/multi_image_picker_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 /**
  * MARK: Share First Screen UI Widget Class
@@ -18,8 +21,169 @@ class ShareFirstScreen extends StatefulWidget {
 class _ShareFirstScreenState extends State<ShareFirstScreen> {
   final FocusNode _nodeText = FocusNode();
 
-  /// Creates the [KeyboardActionsConfig] to hook up the fields
-  /// and their focus nodes to our [FormKeyboardActions].
+  List<Asset> images = <Asset>[];
+  String _error = 'No Error Dectected';
+  bool _permissionReady = false;
+  AppLifecycleListener? _lifecycleListener;
+  static const List<Permission> _permissions = [
+    Permission.storage,
+    Permission.camera
+  ];
+
+  Future<void> _requestPermissions() async {
+    final Map<Permission, PermissionStatus> statues =
+        await _permissions.request();
+    if (statues.values.every((status) => status.isGranted)) {
+      _permissionReady = true;
+    }
+  }
+
+  Future<void> _checkPermissions() async {
+    _permissionReady = (await Future.wait(_permissions.map((e) => e.isGranted)))
+        .every((isGranted) => isGranted);
+  }
+
+  Future<void> _loadAssets() async {
+    if (!_permissionReady) {
+      openAppSettings();
+      return;
+    }
+
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    List<Asset> resultList = <Asset>[];
+    String error = 'No Error Dectected';
+
+    const AlbumSetting albumSetting = AlbumSetting(
+      fetchResults: {
+        PHFetchResult(
+          type: PHAssetCollectionType.smartAlbum,
+          subtype: PHAssetCollectionSubtype.smartAlbumUserLibrary,
+        ),
+        PHFetchResult(
+          type: PHAssetCollectionType.smartAlbum,
+          subtype: PHAssetCollectionSubtype.smartAlbumFavorites,
+        ),
+        PHFetchResult(
+          type: PHAssetCollectionType.album,
+          subtype: PHAssetCollectionSubtype.albumRegular,
+        ),
+        PHFetchResult(
+          type: PHAssetCollectionType.smartAlbum,
+          subtype: PHAssetCollectionSubtype.smartAlbumSelfPortraits,
+        ),
+        PHFetchResult(
+          type: PHAssetCollectionType.smartAlbum,
+          subtype: PHAssetCollectionSubtype.smartAlbumPanoramas,
+        ),
+        PHFetchResult(
+          type: PHAssetCollectionType.smartAlbum,
+          subtype: PHAssetCollectionSubtype.smartAlbumVideos,
+        ),
+      },
+    );
+    const SelectionSetting selectionSetting = SelectionSetting(
+      min: 0,
+      max: 3,
+      unselectOnReachingMax: true,
+    );
+    const DismissSetting dismissSetting = DismissSetting(
+      enabled: true,
+      allowSwipe: true,
+    );
+    final ThemeSetting themeSetting = ThemeSetting(
+      backgroundColor: colorScheme.background,
+      selectionFillColor: colorScheme.primary,
+      selectionStrokeColor: colorScheme.onPrimary,
+      previewSubtitleAttributes: const TitleAttribute(fontSize: 12.0),
+      previewTitleAttributes: TitleAttribute(
+        foregroundColor: colorScheme.primary,
+      ),
+      albumTitleAttributes: TitleAttribute(
+        foregroundColor: colorScheme.primary,
+      ),
+    );
+    const ListSetting listSetting = ListSetting(
+      spacing: 5.0,
+      cellsPerRow: 4,
+    );
+    const AssetsSetting assetsSetting = AssetsSetting(
+      // Set to allow pick videos.
+      supportedMediaTypes: {MediaTypes.video, MediaTypes.image},
+    );
+    final CupertinoSettings iosSettings = CupertinoSettings(
+      fetch: const FetchSetting(album: albumSetting, assets: assetsSetting),
+      theme: themeSetting,
+      selection: selectionSetting,
+      dismiss: dismissSetting,
+      list: listSetting,
+    );
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        selectedAssets: images,
+        iosOptions: IOSOptions(
+          doneButton:
+              UIBarButtonItem(title: 'Confirm', tintColor: colorScheme.primary),
+          cancelButton:
+              UIBarButtonItem(title: 'Cancel', tintColor: colorScheme.primary),
+          albumButtonColor: colorScheme.primary,
+          settings: iosSettings,
+        ),
+        androidOptions: AndroidOptions(
+          actionBarColor: colorScheme.surface,
+          actionBarTitleColor: colorScheme.onSurface,
+          statusBarColor: colorScheme.surface,
+          actionBarTitle: "Select Photo",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: colorScheme.primary,
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      _error = error;
+    });
+  }
+
+  @override
+  void initState() {
+    _requestPermissions();
+    _lifecycleListener = AppLifecycleListener(
+      onResume: _checkPermissions,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _lifecycleListener?.dispose();
+    super.dispose();
+  }
+
+  Widget _buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 300,
+        );
+      }),
+    );
+  }
+
   KeyboardActionsConfig _buildConfig(BuildContext context) {
     return KeyboardActionsConfig(
       keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
@@ -47,7 +211,11 @@ class _ShareFirstScreenState extends State<ShareFirstScreen> {
             //button 2
             (node) {
               return GestureDetector(
-                onTap: () => node.unfocus(),
+                onTap: () {
+                  print("gallery");
+                  // _loadAssets();
+                  // node.unfocus();
+                },
                 child: Container(
                   width: 32,
                   height: 32,
@@ -109,26 +277,47 @@ class _ShareFirstScreenState extends State<ShareFirstScreen> {
   Widget build(BuildContext context) {
     return KeyboardActions(
       config: _buildConfig(context),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.text,
-                focusNode: _nodeText,
-                decoration: InputDecoration(
-                  hintText: "Input Number with Toolbar Buttons",
+      child: Container(
+        // width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 24, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextField(
+                  maxLines: 10,
+                  minLines: 3,
+                  keyboardType: TextInputType.multiline,
+                  focusNode: _nodeText,
+                  decoration: InputDecoration(
+                    hintText: "Share your announcement.",
+                    hintStyle: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: Color(0xFFCBCBCB),
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        height: 1.47,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
                 ),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: "Input Number with Custom Footer",
+                Container(child: Text('Error: $_error')),
+                ElevatedButton(
+                  onPressed: _loadAssets,
+                  child: const Text("Pick images"),
                 ),
-              ),
-            ],
+                Container(
+                  child: _buildGridView(),
+                )
+              ],
+            ),
           ),
         ),
       ),
